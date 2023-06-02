2d65b89787bd14b607d86c2cb220ee8480d37c56
nguyennhatminh230801 <fcarsenal614@gmail.com>
2023-06-02 12:06:40 +0700

    create screen base for app

---
+++ app/src/main/java/com/nhom3/quanlyguixe/screens/parkinglots/ListParkingLotsFragment.java
@@ -0,0 +1,28 @@
+package com.nhom3.quanlyguixe.screens.parkinglots;
+
+import android.view.LayoutInflater;
+
+import com.nhom3.quanlyguixe.databinding.FragmentListParkingLotsBinding;
+import com.nhom3.quanlyguixe.util.base.BaseFragment;
+
+public class ListParkingLotsFragment extends BaseFragment<FragmentListParkingLotsBinding> {
+    @Override
+    public FragmentListParkingLotsBinding inflateViewBinding(LayoutInflater inflater) {
+        return FragmentListParkingLotsBinding.inflate(inflater);
+    }
+
+    @Override
+    protected void initScreenData() {
+
+    }
+
+    @Override
+    protected void addEvent() {
+
+    }
+
+    @Override
+    protected void bindToViewModel() {
+
+    }
+}
---
+++ app/src/main/java/com/nhom3/quanlyguixe/screens/shiftmanager/ListShiftManagerFragment.java
@@ -0,0 +1,28 @@
+package com.nhom3.quanlyguixe.screens.shiftmanager;
+
+import android.view.LayoutInflater;
+
+import com.nhom3.quanlyguixe.databinding.FragmentListShiftManagerBinding;
+import com.nhom3.quanlyguixe.util.base.BaseFragment;
+
+public class ListShiftManagerFragment extends BaseFragment<FragmentListShiftManagerBinding> {
+    @Override
+    public FragmentListShiftManagerBinding inflateViewBinding(LayoutInflater inflater) {
+        return FragmentListShiftManagerBinding.inflate(inflater);
+    }
+
+    @Override
+    protected void initScreenData() {
+
+    }
+
+    @Override
+    protected void addEvent() {
+
+    }
+
+    @Override
+    protected void bindToViewModel() {
+
+    }
+}
