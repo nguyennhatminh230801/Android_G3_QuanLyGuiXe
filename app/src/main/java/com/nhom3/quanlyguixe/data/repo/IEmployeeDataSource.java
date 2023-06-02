035949f2898810024f52fb449090d24568708e46
nguyennhatminh230801 <fcarsenal614@gmail.com>
2023-06-02 20:42:23 +0700

    init data employee screen

---
+++ app/src/main/java/com/nhom3/quanlyguixe/data/repo/IEmployeeDataSource.java
@@ -0,0 +1,17 @@
+package com.nhom3.quanlyguixe.data.repo;
+
+import com.nhom3.quanlyguixe.data.model.Employees;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Single;
+
+public interface IEmployeeDataSource {
+    interface Local {
+        Single<List<Employees>> getAllEmployees();
+        Single<Employees> getEmployeeByID(int employeeID);
+        Single<Long> insertEmployee(Employees employees);
+        Single<Integer> updateEmployee(Employees employees);
+        Single<Integer> deleteEmployee(Employees employees);
+    }
+}
