755cf0e1b938712299187fa0714d35d9cdf492d0
tranvietanh236 <vbibi236@gmail.com>
2023-06-13 17:26:53 +0700

    List Shift Manager

---
+++ app/src/main/java/com/nhom3/quanlyguixe/screens/shiftmanager/AddListShiftManagerFragment.java
@@ -0,0 +1,16 @@
+package com.nhom3.quanlyguixe.screens.shiftmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nhom3.quanlyguixe.R;
+
+public class AddListShiftManagerFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_add_list_shift_manager);
+    }
+}
