b92e1c61bbbfb331fe85b09d63c75430fb3aa2eb
nguyennhatminh230801 <fcarsenal614@gmail.com>
2023-06-18 16:02:31 +0700

    fix bug list parking lots crash

--- app/src/main/java/com/nhom3/quanlyguixe/screens/main/MainViewModel.java
+++ app/src/main/java/com/nhom3/quanlyguixe/screens/main/MainViewModel.java
@@ -1,15 +1,6 @@
 package com.nhom3.quanlyguixe.screens.main;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-
-import com.nhom3.quanlyguixe.data.model.User;
-import com.nhom3.quanlyguixe.data.repo.UserRepository;
 import com.nhom3.quanlyguixe.util.base.BaseViewModel;
-import com.nhom3.quanlyguixe.util.interfaces.IResultListener;
-
-import java.util.List;
-
 import javax.inject.Inject;
 
 import dagger.hilt.android.lifecycle.HiltViewModel;
@@ -17,35 +8,8 @@ import dagger.hilt.android.lifecycle.HiltViewModel;
 @HiltViewModel
 public class MainViewModel extends BaseViewModel {
 
-    private final MutableLiveData<List<User>> listUsers = new MutableLiveData<>();
-
-    private final UserRepository userRepository;
-
     @Inject
-    public MainViewModel(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public MainViewModel() {
     }
 
-    public LiveData<List<User>> getListUsers() {
-        return listUsers;
-    }
-
-    public void getAllUsers() {
-        registerDisposable(
-                executeTaskWithLoading(
-                        userRepository.getUsers(),
-                        new IResultListener<List<User>>() {
-                            @Override
-                            public void onSuccess(List<User> data) {
-                                listUsers.postValue(data);
-                            }
-
-                            @Override
-                            public void onError(Throwable throwable) {
-                                error.postValue(throwable.getMessage());
-                            }
-                        }
-                )
-        );
-    }
 }
